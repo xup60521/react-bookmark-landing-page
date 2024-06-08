862c63184655d5cbe5cd1c1a0731b5fe9a155cbc
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-06-08 11:14:33 +0800

    desktop complete

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,9 @@
+import { Fragment, useState } from "react";
+import IconError from "/images/icon-error.svg"
+
 export default function Contact() {
+    const [input, setInput] = useState("");
+
     return (
         <div className="w-full bg-soft_blue text-whtie flex flex-col items-center py-12">
             <div className="flex flex-col w-[23rem]">
@@ -9,15 +14,43 @@ export default function Contact() {
                     Stay up-to-date with what we’re doing
                 </h3>
                 <div className="flex items-start gap-4">
-                    <div className="flex-grow relative">
-                        <input type="email" className="w-full h-10 rounded px-4 text-xs focus:outline-none" placeholder="Enter your email address" />
-                        <div></div>
-                    </div>
+                    <InputComponent input={input} setInput={setInput} />
                     <button className="text-white bg-soft_red border-2 border-soft_red transition hover:bg-white text-xs px-4 h-10 rounded hover:text-soft_red">
                         Contact Us
                     </button>
                 </div>
             </div>
+        </div>
+    );
+}
+
+function InputComponent(props: {
+    input: string;
+    setInput: React.Dispatch<React.SetStateAction<string>>;
+}) {
+    const { input, setInput } = props;
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    return (
+        <div className="flex-grow relative lg:h-16">
+            <input
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                type="email"
+                className={`w-full h-10 rounded px-4 text-xs focus:outline-none border-2 ${
+                    input !== "" && !emailRegex.test(input)
+                        ? "border-soft_red rounded-b-none"
+                        : ""
+                }`}
+                placeholder="Enter your email address"
+            />
+            {input !== "" && !emailRegex.test(input) && (
+                <Fragment>
+                    <img src={IconError} alt="email error icon" className="absolute right-3 size-5 top-[0.65rem]" />
+                    <div className="h-fit w-full -z-10 bg-soft_red rounded-b text-xs px-3 py-1 text-white font-rubik italic">
+                        Whoops, make sure it's an email
+                    </div>
+                </Fragment>
+            )}
         </div>
     );
 }
